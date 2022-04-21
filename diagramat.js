d89caa199357d75ed828f6fb0b022e7a76b55bc5
maxiii23 <maximiliano.cardenas.hernandez@alumnos.sip.cl>
2022-04-21 11:13:09 -0400

    commit 1

---
+++ diagramat.js
@@ -0,0 +1,49 @@
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+//Fragmento de código 1
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+
+    /*
+    VALIABLES               DATOS
+
+    auntContactInfo     =    ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    
+    
+    */
+}
+/*copy
+Fragmento de código 2
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+ /*
+    VALIABLES               DATOS
+    produce         =       ["manzanas", "naranjas"];
+    frozen          =       ["brócoli", "helado"];
+
+
+        =    ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    
+    
+    */
+
+/*Fragmento de código 3
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+VALIABLES               DATOS
+movieLibrary      =     ["Bambi", "E.T.", "Toy Story"];
+*/
---
+++ return.js
@@ -0,0 +1,5 @@
+function hello() {
+    console.log('hello');
+}
+hello();
+console.log('Dojo');
