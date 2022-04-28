901508eb655ed9180bff14d711a6045afe700ae0
maxiii23 <maximiliano.cardenas.hernandez@alumnos.sip.cl>
2022-04-28 09:06:54 -0400

    IMAGEN_APP_ALGORITMO

--- js/diagramat.js
+++ js/diagramat.js
@@ -1,15 +1,22 @@
 function muestraListaDeCompras() {
-    var produce = ["manzanas", "naranjas"];
-    var frozen = ["brócoli", "helado"];
-    frozen.push("croqueta de papa");
-    console.log(frozen);
+  var produce = ["manzanas", "naranjas"];
+  var frozen = ["brócoli", "helado"];
+  frozen.push("croqueta de papa");
+  console.log(frozen);
 }
 //Fragmento de código 1
 function muestraInformaciónContacto() {
-    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
-    console.log(auntContactInfo);
+  var auntContactInfo = [
+    "Paula",
+    "Smith",
+    "Calle Principal 1234",
+    "St. Louis",
+    "MO",
+    12345,
+  ];
+  console.log(auntContactInfo);
 
-    /*
+  /*
     VALIABLES               DATOS
 
     auntContactInfo     =    ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
@@ -21,12 +28,12 @@ function muestraInformaciónContacto() {
 Fragmento de código 2
 */
 function muestraListaDeCompras() {
-    var produce = ["manzanas", "naranjas"];
-    var frozen = ["brócoli", "helado"];
-    frozen.push("croqueta de papa");
-    console.log(frozen);
+  var produce = ["manzanas", "naranjas"];
+  var frozen = ["brócoli", "helado"];
+  frozen.push("croqueta de papa");
+  console.log(frozen);
 }
- /*
+/*
     VALIABLES               DATOS
     produce         =       ["manzanas", "naranjas"];
     frozen          =       ["brócoli", "helado"];
@@ -38,7 +45,7 @@ function muestraListaDeCompras() {
     */
 
 /*Fragmento de código 3
-*/
+ */
 var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
 movieLibrary.push("Zoro");
 movieLibrary[1] = "Beetlejuice";
@@ -47,3 +54,28 @@ console.log(movieLibrary);
 VALIABLES               DATOS
 movieLibrary      =     ["Bambi", "E.T.", "Toy Story"];
 */
+
+var msg1 = "hello";
+var msg2 = "world!";
+for (var num1 = 1; num1 <= msg1.length; num1++) {
+  for (var num2 = 1; num2 <= msg2.length; num2++) {
+    if (msg1.length == 4) {
+      console.log(msg1 + " " + msg2);
+    }
+  }
+  if (num1 != 4) {
+    console.log(msg1.length + msg2.length);
+  }
+}
+var msg1 = "hello";
+var msg2 = "world!";
+for (var num1 = 1; num1 <= msg1.length; num1++) {
+  for (var num2 = 1; num2 <= msg2.length; num2++) {
+    if (msg1.length == 4) {
+      console.log(msg1 + " " + msg2);
+    }
+  }
+  if (num1 != 4) {
+    console.log(msg1.length + msg2.length);
+  }
+}
